c4332078595be73bbeb5870ce5a4b7430f6283f8
amitir22 <amitir22@gmail.com>
2021-01-03 22:55:34 +0200

    HW2 part E:  - minor fixes in the dry document  - todo: determine how to run experiments (light/heavy) for graph
    dry: (.docx)
     - merged from linoy
    
    players:
     - alpha-beta: (unchanged)
       - starting to outsource stuff to utils
    
     - global time alpha-beta: (unchanged)
       - updated time-limit usage
    
     - minimax: (unchanged)
       - X
    
     - competition:
       - major fixes
       - more transfers to utils
    
     - heavy: (unchanged)
       - minor fixes
    
     - light: (unchanged)
       - minor fixes
    
    search-algos: (unchanged)
       - X
    
    utils:
       - more transfers to utils

--- HW2/players/CompetePlayer.py
+++ HW2/players/CompetePlayer.py
@@ -32,8 +32,8 @@ class Player(AbstractPlayer):
     def __init__(self, game_time, penalty_score):
         # keep the inheritance of the parent's (AbstractPlayer) __init__()
         AbstractPlayer.__init__(self, game_time, penalty_score)
-        self.search_algo = AlphaBeta(Player.heuristic_function, Player.successor_states_of,
-                                     Player.perform_move)
+        self.search_algo = AlphaBeta(utils.heuristic_function, utils.sorted_successor_states_of,
+                                     utils.perform_move)
         self.my_state_list = []
         self.game_time_left = game_time
         self.phase_to_phase_factor = {1: 1/self.SQRT_2, 3: 1/self.SQRT_2, 2: self.SQRT_2}
@@ -136,7 +136,7 @@ class Player(AbstractPlayer):
         assert last_best_move != (0, 0)
 
         # updating the board in the next state and moving the current_state to point at the next
-        self.current_state = Player.perform_move(self.current_state, MY_TURN, next_location)
+        self.current_state = utils.perform_move(self.current_state, MY_TURN, next_location)
         self.my_state_list.append((self.current_state, last_minimax_value, last_best_move))
 
         # todo: remove
@@ -160,7 +160,7 @@ class Player(AbstractPlayer):
         output:
             :return: None
         """
-        self.current_state = Player.perform_move(self.current_state, RIVAL_TURN, pos)
+        self.current_state = utils.perform_move(self.current_state, RIVAL_TURN, pos)
 
     def update_fruits(self, fruits_on_board_dict):
         """Update your info on the current fruits on board (if needed).
@@ -175,146 +175,22 @@ class Player(AbstractPlayer):
 
     ########## helper functions in class ##########
 
-    @staticmethod
-    def is_move_valid(board: np.array, target_location: Tuple[int, int]):
-        if Player.is_location_in_board(board, target_location):
-            is_not_blocked = board[target_location] != BLOCK_CELL
-
-            return is_not_blocked
-        else:
-            return False
-
-    @staticmethod
-    def is_location_in_board(board: np.array, location: Tuple[int, int]):
-        num_rows = len(board)
-        num_cols = len(board[0])
-
-        row, col = location
-
-        return 0 <= row < num_rows and 0 <= col < num_cols
-
-    @staticmethod
-    def m_dist(loc1: Tuple[int, int], loc2: Tuple[int, int]):
-        row1, col1 = loc1
-        row2, col2 = loc2
-
-        return np.abs(row2 - row1) + np.abs(col2 - col1)
-
     ########## helper functions for the search algorithm ##########
 
     @staticmethod
-    def perform_move(state: PlayerState, player_turn: int, target_location: Tuple[int, int]):
-        """
-        input:
-            :param state: the state which we update
-            :param player_turn: if 0 -> moving my player, else (=1) -> moving rival player
-            :param target_location: the target of the move
-        output:
-            :return: the updated state. None if the move given was not valid
-        """
-        next_state = state.duplicate()
+    def get_max_path_lengths(state: PlayerState):
+        g = Player.build_graph_from_state(state)
 
-        if Player.is_move_valid(next_state.board, target_location):
-            if target_location in next_state.fruit_locations and state.fruits_turns_to_live > 0:
-                del next_state.fruit_locations[target_location]
-                next_state.players_scores[player_turn] += next_state.board[target_location]
+        my_loc_component = Player.get_component_contains_location(g, state.my_loc)
+        rival_loc_component = Player.get_component_contains_location(g, state.rival_loc)
 
-            next_state.players_locations[player_turn] = target_location
-            next_state.board[target_location] = BLOCK_CELL
+        my_longest_path_len = Player.dfs_get_longest_path(my_loc_component, state.my_loc)
+        rival_longest_path_len = Player.dfs_get_longest_path(rival_loc_component, state.rival_loc)
 
-            return next_state
-        return None
+        return my_longest_path_len, rival_longest_path_len
 
     @staticmethod
-    def successor_states_of(state: PlayerState):
-        """
-        :param state: the state we expand
-        :return: tuple (a successor state, the direction to the successor)
-        """
-        heuristic_values_of_state = []
-        successor_states = []
-
-        for direction in state.player.directions:
-            player_old_loc = state.players_locations[state.turn]
-            player_new_loc = utils.tup_add(player_old_loc, direction)
-
-            if Player.is_move_valid(state.board, player_new_loc):
-                next_state = Player.perform_move(state, state.turn, player_new_loc)
-
-                next_state.fruits_turns_to_live -= 1
-                next_state.turn = 1 - state.turn  # if rival -> next=me | else next=rival
-
-                h_value = Player.heuristic_function(next_state)
-
-                heuristic_values_of_state.append(h_value)
-                successor_states.append((next_state, direction))
-
-        sorted_indices = np.argsort(heuristic_values_of_state)
-        returned_successor_states = []
-
-        for index in sorted_indices:
-            returned_successor_states.append(successor_states[index])
-
-        return returned_successor_states
-
-    @staticmethod
-    def heuristic_function(state: PlayerState):
-        a = 2  # todo: change
-
-        my_row, my_col = state.players_locations[state.turn]
-
-        score_available_cells = 1
-        score_blocked_cells = 1
-
-        for row in range(my_row - a, my_row + a + 1):
-            for col in range(my_col - a, my_col + a + 1):
-                if (row, col) != (my_row, my_col):
-                    if Player.is_location_in_board(state.board, (row, col)):
-                        cell_value = state.board[(row, col)]
-
-                        if cell_value > 0:
-                            score_available_cells += cell_value
-                        elif cell_value < 0:
-                            score_blocked_cells -= cell_value
-                        else:  # cell_value = 0
-                            score_available_cells += 1
-                    else:
-                        score_blocked_cells += 1
-
-        current_min_fruit_dist = np.inf
-        current_min_fruit_score = 0
-
-        for fruit_loc in state.fruit_locations:
-            fruit_score = state.board[fruit_loc]
-
-            my_fruit_dist = Player.m_dist(state.my_loc, fruit_loc)
-            rival_fruit_dist = Player.m_dist(state.rival_loc, fruit_loc)
-
-            is_fruit_viable = my_fruit_dist <= rival_fruit_dist and my_fruit_dist < state.fruits_turns_to_live
-
-            if is_fruit_viable and (my_fruit_dist, fruit_score) < (current_min_fruit_dist, current_min_fruit_score):
-                current_min_fruit_dist, current_min_fruit_score = my_fruit_dist, fruit_score
-
-        score_closest_fruit_m_dist = current_min_fruit_dist
-        score_adversary_m_dist = Player.m_dist(state.my_loc, state.rival_loc) + \
-                                 np.linalg.norm(np.array(state.my_loc) - np.array(state.rival_loc))
-
-        possible_loc1 = my_row, my_col + 1
-        possible_loc2 = my_row, my_col - 1
-        possible_loc3 = my_row + 1, my_col
-        possible_loc4 = my_row - 1, my_col
-
-        is_not_hole_state = int(Player.is_move_valid(state.board, possible_loc1) or
-                                Player.is_move_valid(state.board, possible_loc2) or
-                                Player.is_move_valid(state.board, possible_loc3) or
-                                Player.is_move_valid(state.board, possible_loc4))
-
-        # todo: check if need to add '+1' in previous players
-        return is_not_hole_state * ((score_available_cells / score_blocked_cells) /
-                                    (score_closest_fruit_m_dist + score_adversary_m_dist + 1))
-
-    @staticmethod
-    def asdf(state: PlayerState):
+    def build_graph_from_state(state: PlayerState):
         board = state.board
         g = nx.Graph()
 
@@ -323,6 +199,7 @@ class Player(AbstractPlayer):
 
         g.add_nodes_from([(row, col) for row in range(num_rows) for col in range(num_cols)])
 
+        # connecting the nodes
         for row in range(num_rows):
             for col in range(num_cols):
                 loc = row, col
@@ -335,52 +212,46 @@ class Player(AbstractPlayer):
                 if row + 1 < num_rows:
                     g.add_edge(loc, upwards)
 
+        # removing the blocked cells from the graph
         for row in range(num_rows):
             for col in range(num_cols):
                 loc = row, col
-                if board[loc] in [BLOCK_CELL]:
+                if loc not in [state.my_loc, state.rival_loc] and board[loc] == BLOCK_CELL:
                     g.remove_node(loc)
 
-        my_loc_component = Player.get_component_contains_location(g, state.my_loc)
-        rival_loc_component = Player.get_component_contains_location(g, state.rival_loc)
-
-        my_target_vertex = np.argmax(Player.m_dist_component_nodes(my_loc_component, state.my_loc))
-        rival_target_vertex = np.argmax(Player.m_dist_component_nodes(rival_loc_component, state.rival_loc))
-
-        # todo: not good enough, because maybe i don't need to set my target so far away... maybe consider DFS-post-ord.
-        # idea: consider iterating over the nodes of odd-rank (rank is odd)
-        my_longest_path = Player.get_longest_path(state.my_loc, my_loc_component, my_target_vertex)
-        rival_longest_path = Player.get_longest_path(state.rival_loc, rival_loc_component, rival_target_vertex)
-
-        # todo: divide to components
-        # todo: locate component of my_loc
-        # todo: suspicious for removal: vertices in my_loc component which aren't my_loc and is of rank = 1
-        # if there are more than 1, let's say... 3? then first try to remove one of them at a time and check if
-        # eulerian, if found then remove and return eulerian path length, if still fails try removing 2 at a time and
-        # so on...
-
-        return my_longest_path, rival_longest_path
-
-    @staticmethod
-    def get_longest_path(loc: Tuple[int, int], loc_component: nx.Graph, target_vertex_index):
-        target_vertex = loc_component.nodes[target_vertex_index]
-
-        return max((path for path in nx.all_simple_paths(loc_component, loc, target_vertex)))
-
-    @staticmethod
-    def m_dist_component_nodes(my_loc_component: nx.Graph, src: Tuple[int, int]):
-        return [Player.m_dist(src, loc) for node, loc in my_loc_component.nodes(data=True)]
+        return g
 
     @staticmethod
     def get_component_contains_location(g: nx.Graph, loc: Tuple[int, int]):
         return g.subgraph(set(nx.algorithms.components.node_connected_component(g, loc))).copy()
 
+    @staticmethod
+    def dfs_get_longest_path(g: nx.Graph, src):
+        successors = nx.dfs_successors(g, src)
+
+        return Player.recursive_get_longest_path(successors, src)
+
+    @staticmethod
+    def recursive_get_longest_path(node_to_successors: dict, src):
+        if src not in node_to_successors:
+            return 0
+
+        successors = node_to_successors[src]
+
+        list_of_successor_path_length = []
+
+        for successor in successors:
+            list_of_successor_path_length.append(1 + Player.recursive_get_longest_path(node_to_successors, successor))
+
+        return max(list_of_successor_path_length)
+
 
 # TODO: delete
 def main():
-    board = np.array([[1, -1,  2],
-                      [0,  0,  0],
-                      [0,  0,  0]])
+    board = np.array([[1, -1, -1,  2],
+                      [0,  0, -1,  0],
+                      [0, -1, -1,  0],
+                      [0,  0, -1,  0]])
     my_loc = np.where(board == P1_CELL)
     my_loc = my_loc[0][0], my_loc[1][0]
     rival_loc = np.where(board == P2_CELL)
--- HW2/utils.py
+++ HW2/utils.py
@@ -15,6 +15,9 @@ MY_TURN = 0
 RIVAL_TURN = 1
 
 
+# State model:
+
+
 @dataclasses.dataclass(init=True)
 class PlayerState:
     turn: int
@@ -61,11 +64,153 @@ class PlayerState:
         return copy.deepcopy(self)
 
 
+# Custom major functions:
+
+def perform_move(state: PlayerState, player_turn: int, target_location: tuple):
+    """
+    input:
+        :param state: the state which we update
+        :param player_turn: if 0 -> moving my player, else (=1) -> moving rival player
+        :param target_location: the target of the move
+    output:
+        :return: the updated state. None if the move given was not valid
+    """
+    next_state = state.duplicate()
+
+    if is_move_valid(next_state.board, target_location):
+        if target_location in next_state.fruit_locations and state.fruits_turns_to_live > 0:
+            del next_state.fruit_locations[target_location]
+            next_state.players_scores[player_turn] += next_state.board[target_location]
+
+        next_state.players_locations[player_turn] = target_location
+        next_state.board[target_location] = BLOCK_CELL
+
+        return next_state
+    return None
+
+
+def sorted_successor_states_of(state: PlayerState):
+    """
+    :param state: the state we expand
+    :return: tuple (a successor state, the direction to the successor)
+    """
+    heuristic_values_of_state = []
+    successor_states = []
+
+    for direction in get_directions():
+        player_old_loc = state.players_locations[state.turn]
+        player_new_loc = tup_add(player_old_loc, direction)
+
+        if is_move_valid(state.board, player_new_loc):
+            next_state = perform_move(state, state.turn, player_new_loc)
+
+            next_state.fruits_turns_to_live -= 1
+            next_state.turn = 1 - state.turn  # if rival -> next=me | else next=rival
+
+            h_value = heuristic_function(next_state)
+
+            heuristic_values_of_state.append(h_value)
+            successor_states.append((next_state, direction))
+
+    sorted_indices = np.argsort(heuristic_values_of_state)
+    sorted_successor_states = []
+
+    for index in sorted_indices:
+        sorted_successor_states.append(successor_states[index])
+
+    return sorted_successor_states
+
+
+def heuristic_function(state: PlayerState):
+    a = 2  # todo: change
+
+    my_row, my_col = state.my_loc
+
+    score_available_cells = 1
+    score_blocked_cells = 1
+
+    for row in range(my_row - a, my_row + a + 1):
+        for col in range(my_col - a, my_col + a + 1):
+            if (row, col) != (my_row, my_col):
+                if is_location_in_board(state.board, (row, col)):
+                    cell_value = state.board[(row, col)]
+
+                    if cell_value > 0:
+                        score_available_cells += cell_value
+                    elif cell_value < 0:
+                        score_blocked_cells -= cell_value
+                    else:  # cell_value = 0
+                        score_available_cells += 1
+                else:
+                    score_blocked_cells += 1
+
+    current_min_fruit_dist = np.inf
+    current_min_fruit_score = 0
+
+    for fruit_loc in state.fruit_locations:
+        fruit_score = state.board[fruit_loc]
+
+        my_fruit_dist = m_dist(state.my_loc, fruit_loc)
+        rival_fruit_dist = m_dist(state.rival_loc, fruit_loc)
+
+        is_fruit_viable = my_fruit_dist <= rival_fruit_dist and my_fruit_dist < state.fruits_turns_to_live
+
+        if is_fruit_viable and (my_fruit_dist, fruit_score) < (current_min_fruit_dist, current_min_fruit_score):
+            current_min_fruit_dist, current_min_fruit_score = my_fruit_dist, fruit_score
+
+    score_closest_fruit_m_dist = current_min_fruit_dist
+    score_adversary_m_dist = m_dist(state.my_loc, state.rival_loc) + \
+                             np.linalg.norm(np.array(state.my_loc) - np.array(state.rival_loc))
+
+    possible_loc1 = my_row, my_col + 1
+    possible_loc2 = my_row, my_col - 1
+    possible_loc3 = my_row + 1, my_col
+    possible_loc4 = my_row - 1, my_col
+
+    is_not_hole_state = int(is_move_valid(state.board, possible_loc1) or
+                            is_move_valid(state.board, possible_loc2) or
+                            is_move_valid(state.board, possible_loc3) or
+                            is_move_valid(state.board, possible_loc4))
+
+    # todo: check if need to add '+1' in previous players
+    return is_not_hole_state * ((score_available_cells / score_blocked_cells) /
+                                (score_closest_fruit_m_dist + score_adversary_m_dist + 1))
+
+
+# Custom helper functions:
+
+def is_move_valid(board: np.array, target_location: tuple):
+    if is_location_in_board(board, target_location):
+        is_not_blocked = board[target_location] != BLOCK_CELL
+
+        return is_not_blocked
+    else:
+        return False
+
+
+def is_location_in_board(board: np.array, location: tuple):
+    num_rows = len(board)
+    num_cols = len(board[0])
+
+    row, col = location
+
+    return 0 <= row < num_rows and 0 <= col < num_cols
+
+
+def m_dist(loc1: Tuple[int, int], loc2: Tuple[int, int]):
+    row1, col1 = loc1
+    row2, col2 = loc2
+
+    return np.abs(row2 - row1) + np.abs(col2 - col1)
+
+
 def get_directions():
     """Returns all the possible directions of a player in the game as a list of tuples.
     """
     return [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
+
+# Segel functions:
 
 def tup_add(t1, t2):
     """
