dee402d927b8c760da73b61d139a63fc570ffc87
amitir22 <amitir22@gmail.com>
2021-01-19 01:18:56 +0200

    HW3:  - data-set handler fix

--- HW3/data_set_handler.py
+++ HW3/data_set_handler.py
@@ -1,11 +1,29 @@
+import pandas
+
+
 class DataSetHandler:
-    source_path: str
+    train_path: str
+    test_path: str
 
-    def __init__(self, path: str):
-        self.source_path = path
+    def __init__(self,
+                 train_path: str = 'data_sets/train.csv',
+                 test_path: str = 'prediction_tests/test.csv'):
+        self.train_path = train_path
+        self.test_path = test_path
 
-    def bulk_read(self, input_stream, bulk_size: int = 1):
-        pass
+    def read_train_data(self):
+        return pandas.read_csv(self.train_path)
 
-    def read_all(self):
-        pass
+    def read_test_data(self):
+        return pandas.read_csv(self.test_path)
+
+
+def test_read_all():
+    data_set_handler = DataSetHandler()
+
+    return data_set_handler.read_train_data()
+
+
+if __name__ == '__main__':
+    dataframe = test_read_all()
+    pass
